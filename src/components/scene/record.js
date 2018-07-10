9e0fbe00de418563436ee4e27180c420524b6529
ywf2661 <37321042+ywf2661@users.noreply.github.com>
2018-07-10 13:42:06 +0900

    Add files via upload

---
+++ src/components/scene/record.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Dimensions,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Alert,
+  View
+} from 'react-native';
+import { RNCamera } from 'react-native-camera';
+
+export default class record extends Component {
+
+  render() {
+
+    return(
+      <View>
+      
+
+      <Text> 출석기록 확인 페이지 입니다.</Text>
+      </View>
+    )
+
+  }
+
+ 
+}
